9a924ac615ff78fc1f13df48901a4d8b027c16c0
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-10-21 09:58:13 +0800

    MongoDB 6-21: Creating Model Instances

--- test/create_test.js
+++ test/create_test.js
@@ -1,9 +1,10 @@
 const assert = require('assert');
+const User = require('../src/user');
 
 describe('Creating Records', () => {
   
     it('Saves a user', () => {
-      assert(1 + 1 === 2);
+      const joe = new User({ name: 'Joe' });
     });
 
 });
