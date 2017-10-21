ded685a9ff133e0a3bb9cc54c47651edfc4fadee
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-10-21 13:24:09 +0800

    MongoDB 6-23: Dropping Collections

--- test/test_helper.js
+++ test/test_helper.js
@@ -4,3 +4,8 @@ mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
   .once('open', () => console.log("Good to go!"))
   .on('error', (error) => console.warn('Warning ', error));
+
+
+beforeEach(() => {
+  mongoose.connection.collections.users.drop();
+})
