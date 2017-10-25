cf72d0e325676837cc1592e1431a5d8fc45e7c81
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-10-25 22:56:23 +0800

    MongoDB 7-43: Validation with a Validator Function

--- test/validation_test.js
+++ test/validation_test.js
@@ -11,4 +11,12 @@ describe('Validating Records', () => {
     assert(message === 'Name is required');
   });
 
+  it('Requires a user name longer than 2 characters', () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const  { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than 2 characters');
+  });
+
 });
