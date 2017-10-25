e8e8e9cfe422eb60258bf9ffe76978c2aacda650
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-10-25 22:47:00 +0800

    MongoDB 7-42: Requiring Attributes on a Model

--- test/validation_test.js
+++ test/validation_test.js
@@ -4,7 +4,11 @@ const User = require('../src/user');
 describe('Validating Records', () => {
 
   it('Requires a user name', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
 
+    assert(message === 'Name is required');
   });
 
 });
