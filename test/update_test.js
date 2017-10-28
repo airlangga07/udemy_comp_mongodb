31eb3b04caaee6c3dd32ab2affc1cbc120176f5e
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-10-28 13:09:35 +0800

    MongoDB 8-53: Fixing Update Test

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating a Record', () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save().then(() => done());
   });
 
@@ -41,11 +41,11 @@ describe('Updating a Record', () => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
   });
 
-  xit('A User can have their post count incremented by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } } )
+  it('A User can have their post count incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { likes: 1 } } )
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 1);
         done();
       });
   });
