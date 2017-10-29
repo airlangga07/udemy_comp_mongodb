c30f6a5d8d0c8818f55b0d1bb5900673fe682a5a
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-10-29 18:51:25 +0800

    MongoDB 9-59: Wiring Up Has Many and Has One Relations

--- test/association_test.js
+++ test/association_test.js
@@ -4,6 +4,20 @@ const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
 describe('Associations', () => {
+  let joe, blogPost, comment;
+  
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep! It is Great!' });
+    comment = new Comment({ content: 'Congrats on great post!' });
+
+    // referencing the blogpost to user, one-to-many array, so we push
+    joe.blogPosts.push(blogPost);
+    // referencing the comment to blogpost, one-to-many array, so we push
+    blogPost.comments.push(comment);
+    // referencing the comment to user, one-to-one array, so we just initialize the object to prop
+    comment.user = joe;
+  });
 
   it('', (done) => {
 
