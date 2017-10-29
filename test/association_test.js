9e4d884942ffda64b2290551d097e393daee4e85
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-10-29 18:41:03 +0800

    MongoDB 9-58: Test Setup for Associations

---
+++ test/association_test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
+
+describe('Associations', () => {
+
+  it('', (done) => {
+
+  });
+
+});
