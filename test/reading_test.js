57be6bb894353dad87bf642371064c49fc90db85
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-10-31 22:22:13 +0800

    MongoDB 11-69: Sorting Collections

--- test/reading_test.js
+++ test/reading_test.js
@@ -28,6 +28,16 @@ describe('Reading users out of database', () => {
   });
 
   it('can skip and limit the result set', (done) => {
-    done();
+    User.find({})
+        .sort({ name: 1 })
+        .skip(1)
+        .limit(2)
+        .then((users) => {
+          assert(users.length === 2);
+          assert(users[0].name === 'Joe');
+          assert(users[1].name === 'Maria');
+          done();
+        });
   }); 
+
 });
